369406ec6389163b072ae829dee7aaafeea7f94f
Soliheen Farooq Khan <soliheen.farooqkhan@u-next.com>
2025-04-17 20:44:12 +0530

    addedCommonUtilsAndImageTypes

---
+++ app/common/commonUtils.ts
@@ -0,0 +1,25 @@
+// app/common/common.ts
+import { StyleSheet } from 'react-native';
+import Toast, { ToastType } from 'react-native-toast-message';
+
+// Reusable global styles
+export const commonStyles = StyleSheet.create({
+  errorText: {
+    color: 'red',
+    fontSize: 12,
+    marginBottom: 8,
+    marginLeft: 4,
+  },
+});
+
+//  Reusable toast function
+export function showToaster(message: string, type: ToastType = 'success') {
+  Toast.show({
+    type,
+    text1: message,
+    position: 'top',
+    visibilityTime: 4000,
+    autoHide: true,
+    topOffset: 60,
+  });
+}

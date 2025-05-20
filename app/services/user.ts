0aa7196f5599e6acf5d6339066f1f3ec111a7f97
oph <fshah@mindstask.com>
2025-05-20 15:54:20 +0530

    feat - api service dummy setup

---
+++ app/services/user.ts
@@ -0,0 +1,14 @@
+import { UpdateProfileData, User } from '../interface/api';
+import ApiService from './api';
+
+class UserService {
+  static async getProfile(): Promise<User> {
+    return ApiService.get<User>('/user/profile');
+  }
+
+  static async updateProfile(data: UpdateProfileData): Promise<User> {
+    return ApiService.put<User>('/user/profile', data);
+  }
+}
+
+export default UserService; 
